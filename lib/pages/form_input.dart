d4c34381e98ff16eb0a133bfe11e8237e3cc506b
Barbecue23 <sunsanasupphong_j@silpakorn.com>
2024-03-16 01:04:39 +0700

    final

--- lib/pages/form_input.dart
+++ lib/pages/form_input.dart
@@ -15,83 +15,92 @@ class _FormInputState extends State<FormInput> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Center(child: Text('หมวกคัดสรรเลือกบ้าน')),
+        backgroundColor: Colors.red,
+        title: Center(child: Text('หมวกคัดสรรเลือกบ้าน', style: TextStyle(color: Colors.white))),
       ),
-      body: Padding(
-        padding: EdgeInsets.all(20.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Icon(Icons.settings),
-              ],
-            ),
-            SizedBox(height: 20.0),
-            Row(
-              children: [
-                Icon(Icons.person),
-                SizedBox(width: 10),
-                Text('ป้อนจำนวนคน'),
-              ],
-            ),
-            SizedBox(height: 20.0),
-            TextField(
-              controller: _controller,
-              keyboardType: TextInputType.number,
-              decoration: InputDecoration(
-                labelText: 'ป้อนจำนวนคน',
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/images/4king_backgroud.png'),
+            fit: BoxFit.cover
+          )
+        ),
+        child: Padding(
+          padding: EdgeInsets.all(20.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image.asset('assets/images/โลโก้.png', width: 200, height: 200),
+                ],
               ),
-              onChanged: (value) {
-                setState(() {
-                  number = int.tryParse(value) ?? 0;
-                });
-              },
-            ),
-            SizedBox(height: 20.0),
-            Row(
-              children: [
-                Icon(Icons.person),
-                SizedBox(width: 10),
-                Text('ป้อนชื่อ'),
-              ],
-            ),
-            SizedBox(height: 20.0),
-            Expanded(
-              child: ListView.builder(
-                itemCount: number,
-                itemBuilder: (context, index) {
-                  return TextField(
-                    onChanged: (value) {
-                      setState(() {
-                        if (names.length <= index) {
-                          names.add(value);
-                        } else {
-                          names[index] = value;
-                        }
-                      });
-                    },
-                    decoration: InputDecoration(
-                      labelText: 'ป้อนชื่อคนที่ ${index + 1}',
-                    ),
-                  );
+              SizedBox(height: 20.0),
+              Row(
+                children: [
+                  Icon(Icons.person),
+                  SizedBox(width: 10),
+                  Text('ป้อนจำนวนคน'),
+                ],
+              ),
+              SizedBox(height: 20.0),
+              TextField(
+                controller: _controller,
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                  labelText: 'ป้อนจำนวนคน',
+                ),
+                onChanged: (value) {
+                  setState(() {
+                    number = int.tryParse(value) ?? 0;
+                  });
                 },
               ),
-            ),
-            SizedBox(height: 20.0),
-            Center(
-              child: ElevatedButton(
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => Home(names: names)),
-                  );
-                },
-                child: Text('บันทึก'),
+              SizedBox(height: 20.0),
+              Row(
+                children: [
+                  Icon(Icons.person),
+                  SizedBox(width: 10),
+                  Text('ป้อนชื่อ'),
+                ],
               ),
-            ),
-          ],
+              SizedBox(height: 20.0),
+              Expanded(
+                child: ListView.builder(
+                  itemCount: number,
+                  itemBuilder: (context, index) {
+                    return TextField(
+                      onChanged: (value) {
+                        setState(() {
+                          if (names.length <= index) {
+                            names.add(value);
+                          } else {
+                            names[index] = value;
+                          }
+                        });
+                      },
+                      decoration: InputDecoration(
+                        labelText: 'ป้อนชื่อคนที่ ${index + 1}',
+                      ),
+                    );
+                  },
+                ),
+              ),
+              SizedBox(height: 20.0),
+              Center(
+                child: ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => Home(names: names)),
+                    );
+                  },
+                  child: Text('บันทึก'),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -7,85 +7,101 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // แบ่งชื่อออกเป็น 4 กลุ่ม
     List<List<String>> dividedNames = _divideNames(names, 4);
 
     return Scaffold(
       appBar: AppBar(
-        title: Text('Home'),
+        backgroundColor: Colors.red,
+        title: Text('Home', style: TextStyle(color: Colors.white)),
       ),
-      body: Center(
-        child: SingleChildScrollView(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              SizedBox(height: 20),
-              Text('ข้อมูลที่รับมา:'),
-              SizedBox(height: 10),
-              // แสดงชื่อแต่ละบ้าน
-              Column(
-                children: dividedNames.asMap().entries.map((entry) {
-                  int houseIndex = entry.key + 1;
-                  List<String> houseNames = entry.value;
-                  String houseName = '';
-                  switch (houseIndex) {
-                    case 1:
-                      houseName = 'ประชาชื่น';
-                      break;
-                    case 2:
-                      houseName = 'อินทร';
-                      break;
-                    case 3:
-                      houseName = 'กนกอาชีวะ';
-                      break;
-                    case 4:
-                      houseName = 'บูรณพล';
-                      break;
-                    default:
-                      houseName = 'บ้านที่ $houseIndex';
-                  }
-                  return Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Row(
-                        children: [
-                          Text(houseName),
-                          SizedBox(width: 10),
-                          Icon(Icons.home),
-                        ],
-                      ),
-                      Card(
-                        child: Column(
-                          children: houseNames.map((name) {
-                            return ListTile(
-                              title: Text(name),
-                            );
-                          }).toList(),
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/images/backgroud.png'),
+            fit: BoxFit.cover
+          )
+        ),
+        child: Center(
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Image.asset('assets/images/ประชาชื่น.png', width: 100, height: 100),
+                    SizedBox(width: 10),
+                    Image.asset('assets/images/อินทร.png', width: 100, height: 100),
+                    SizedBox(width: 10),
+                    Image.asset('assets/images/กนก.png', width: 100, height: 100),
+                    SizedBox(width: 10),
+                    Image.asset('assets/images/บุรณพนธ์.png', width: 100, height: 100),
+                  ],
+                ),
+                SizedBox(height: 20),
+                Text('แบ่งได้บ้านดังนี้:'),
+                SizedBox(height: 10),
+                Column(
+                  children: dividedNames.asMap().entries.map((entry) {
+                    int houseIndex = entry.key + 1;
+                    List<String> houseNames = entry.value;
+                    String houseName = '';
+                    switch (houseIndex) {
+                      case 1:
+                        houseName = 'ประชาชื่น';
+                        break;
+                      case 2:
+                        houseName = 'อินทร';
+                        break;
+                      case 3:
+                        houseName = 'กนกอาชีวะ';
+                        break;
+                      case 4:
+                        houseName = 'บุรณพนธ์';
+                        break;
+                      default:
+                        houseName = 'บ้านที่ $houseIndex';
+                    }
+                    return Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Row(
+                          children: [
+                            SizedBox(width: 10),
+                            Icon(Icons.home),
+                            SizedBox(width: 10),
+                            Text(houseName),
+                          ],
                         ),
-                      ),
-                    ],
-                  );
-                }).toList(),
-              ),
-            ],
+                        Card(
+                          color: Colors.red[400],
+                          child: Column(
+                            children: houseNames.map((name) {
+                              return ListTile(
+                                title: Text(name),
+                              );
+                            }).toList(),
+                          ),
+                        ),
+                      ],
+                    );
+                  }).toList(),
+                ),
+              ],
+            ),
           ),
         ),
       ),
     );
   }
 
-  // เมทอดสำหรับแบ่ง List ออกเป็นกลุ่มๆ
   List<List<String>> _divideNames(List<String> names, int numOfGroups) {
     List<List<String>> dividedList = [];
     int numOfNamesInGroup = (names.length / numOfGroups).ceil();
-    int numOfNamesInGroup2 = (names.length/ numOfGroups).floor();
     int start = 0;
 
     for (int i = 0; i < numOfGroups; i++) {
-      if (names.length == 5 || names.length == 6) {
-        numOfNamesInGroup = numOfNamesInGroup2;
-      }
       int end = start + numOfNamesInGroup;
       if (end > names.length) {
         end = names.length;
@@ -94,16 +110,11 @@ class Home extends StatelessWidget {
       start = end;
     }
 
-    for(int i = 0; i < 2; i++) {
-      if(names.length == 5 ) {
-        dividedList[0].add(names[4]);
-        break;
-      }else if(names.length == 6){
-        dividedList[0].add(names[4]);
-        dividedList[1].add(names[5]);
-        break;
-      }
-      
+    if (names.length == 5) {
+      dividedList[0].add(names[4]);
+    } else if (names.length == 6) {
+      dividedList[0].add(names[4]);
+      dividedList[1].add(names[5]);
     }
     return dividedList;
   }
